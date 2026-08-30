72ecf5af037e66d89ee4adacd7acc0ca7b95ae47
Mrcat16 <Mrcat16@://github.com>
2026-08-30 01:59:07 -0500

    first commit

---
+++ codigo_de_pagina_web.js
@@ -0,0 +1,4 @@
+document.getElementById("titulo").textContent = "Gerat De Rivia";
+document.getElementById("sustitulo").textContent = "Geralt de Rivia es un personaje de la saga de videojuegos protagonista de los 3 primeros juegos es un experto en el combate cuerpo a cuerpo con espadas";
+document.getElementById("imagen").src = "Geralt.jpg";
+document.getElementById("sustitulos2").textContent = "Es fomoso en su mundo por ser un grand cazador de mostruos para a la vez por 'supuestamente matar al un ret' el cual fue un crimer que no cometio";
